ffedabc9f6357184c8bda9c7f16bfac8bcc934c9
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2014-03-26 15:14:35 +0200

    improve cacheddb

--- notmuch-gui/CachedDB.cs
+++ notmuch-gui/CachedDB.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using NM = NotMuch;
+using System.Threading;
 
 namespace NotMuchGUI
 {
@@ -11,6 +12,7 @@ namespace NotMuchGUI
 		static DateTime s_unusedDateStamp;
 		static int s_readers;
 		static int s_writers;
+		static ReaderWriterLockSlim m_rwLock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
 
 		public static event Action<bool> DBOpenEvent;
 		public static event Action DBCloseEvent;
@@ -27,101 +29,85 @@ namespace NotMuchGUI
 		public CachedDB(bool writable)
 		{
 			m_writable = writable;
-			bool notify = false;
+
+			if (writable)
+				m_rwLock.EnterWriteLock();
+			else
+				m_rwLock.EnterReadLock();
 
 			lock (s_lock)
 			{
-				if (s_writers > 0)
-					throw new Exception();
-
-				if (writable && s_readers > 0)
-					throw new Exception();
-
 				if (s_unusedDB != null)
 				{
 					if (writable)
 					{
-						Console.WriteLine("flush cached DB");
 						s_unusedDB.Dispose();
 						s_unusedDB = null;
 					}
 					else
 					{
-						Console.WriteLine("returning cached DB");
 						this.Database = s_unusedDB;
 						s_unusedDB = null;
 					}
 				}
 
-				if (this.Database == null)
-				{
-					NM.Status status;
-
-					var mode = writable ? NM.DatabaseMode.READ_WRITE : NM.DatabaseMode.READ_ONLY;
-
-					Console.WriteLine("returning new DB {0}", mode);
-
-					var db = NM.Database.Open(MainClass.DatabasePath, mode, out status);
-					if (db == null)
-						throw new Exception();
-
-					this.Database = db;
-				}
+				if (s_writers == 0 && s_readers == 0 && DBOpenEvent != null)
+					DBOpenEvent(writable);
 
 				if (writable)
-				{
-					if (s_writers++ == 0)
-						notify = true;
-				}
+					s_writers++;
 				else
-				{
-					if (s_readers++ == 0)
-						notify = true;
-				}
+					s_readers++;
 			}
 
-			if (notify)
-				DBOpenEvent(writable);
+			if (this.Database == null)
+			{
+				NM.Status status;
+
+				var mode = writable ? NM.DatabaseMode.READ_WRITE : NM.DatabaseMode.READ_ONLY;
+
+				var db = NM.Database.Open(MainClass.DatabasePath, mode, out status);
+				if (db == null)
+					throw new Exception();
+
+				this.Database = db;
+			}
 		}
 
 		public void Dispose()
 		{
 			GC.SuppressFinalize(this);
 
-			bool notify = false;
-
 			lock (s_lock)
 			{
 				if (m_writable == false && s_unusedDB == null)
 				{
-					Console.WriteLine("caching DB");
 					s_unusedDB = this.Database;
 					s_unusedDateStamp = DateTime.UtcNow;
 					this.Database = null;
 
 					GLib.Timeout.Add(1000, DisposeCheckTimeout);
 				}
-				else
-				{
-					Console.WriteLine("discarding DB");
-					this.Database.Dispose();
-					this.Database = null;
-				}
 
 				if (m_writable)
-				{
-					if (--s_writers == 0)
-						notify = true;
-				}
+					s_writers--;
 				else
-				{
-					if (--s_readers == 0)
-						notify = true;
-				}
+					s_readers--;
+
+				if (s_writers == 0 && s_readers == 0 && DBCloseEvent != null)
+					DBCloseEvent();
 			}
 
-			if (notify)
-				DBCloseEvent();
+			if (this.Database != null)
+			{
+				this.Database.Dispose();
+				this.Database = null;
+			}
+
+			if (m_writable)
+				m_rwLock.ExitWriteLock();
+			else
+				m_rwLock.ExitReadLock();
 		}
 
 		~CachedDB()
@@ -142,7 +128,6 @@ namespace NotMuchGUI
 
 				if (diff.TotalMilliseconds >= 1000)
 				{
-					Console.WriteLine("disposing by timeout");
 					s_unusedDB.Dispose();
 					s_unusedDB = null;
 				}
