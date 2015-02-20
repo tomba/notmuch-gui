2718d3d135c6ae557f49391b528c5f66e47d220a
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-02-20 10:41:26 +0200

    CmdHelpers: use GLib

--- notmuch-gui/CmdHelpers.cs
+++ notmuch-gui/CmdHelpers.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.Text;
 
 namespace NotMuchGUI
@@ -14,121 +13,10 @@ namespace NotMuchGUI
 				GLib.SpawnFlags.SearchPath, null, out child);
 		}
 
-		public static bool RunCmd(string cmd, string args, out string stdout, out string stderr,
-		                          out int ret, out string err, int maxbuf = 100000)
+		public static bool RunCmd(string cmd, string args, out string stdout, out string stderr, out int ret)
 		{
-			err = null;
-
-			var psi = new ProcessStartInfo(cmd, args)
-			{
-				CreateNoWindow = true,
-				UseShellExecute = false,
-				RedirectStandardError = true,
-				RedirectStandardOutput = true,
-			};
-
-			var errSb = new StringBuilder();
-			var stdSb = new StringBuilder();
-
-			var lockOb = new object();
-			bool killed = false;
-
-			using (var p = new Process())
-			{
-				p.StartInfo = psi;
-
-				p.ErrorDataReceived += (sender, e) =>
-				{
-					if (string.IsNullOrEmpty(e.Data))
-						return;
-
-					lock (lockOb)
-					{
-						if (killed)
-							return;
-
-						if (errSb.Length + e.Data.Length > maxbuf)
-						{
-							killed = true;
-
-							try
-							{
-								p.Kill();
-							}
-							catch
-							{
-							}
-
-							return;
-						}
-
-						errSb.AppendLine(e.Data);
-					}
-				};
-
-				p.OutputDataReceived += (sender, e) =>
-				{
-					if (string.IsNullOrEmpty(e.Data))
-						return;
-
-					lock (lockOb)
-					{
-						if (killed)
-							return;
-
-						if (stdSb.Length + e.Data.Length > maxbuf)
-						{
-							killed = true;
-
-							try
-							{
-								p.Kill();
-							}
-							catch
-							{
-							}
-
-							return;
-						}
-
-						stdSb.AppendLine(e.Data);
-					}
-				};
-
-				try
-				{
-					p.Start();
-				}
-				catch (Exception e)
-				{
-					err = e.Message;
-					ret = 0;
-					stdout = null;
-					stderr = null;
-					return false;
-				}
-
-				p.BeginOutputReadLine();
-				p.BeginErrorReadLine();
-
-				p.WaitForExit();
-
-				if (killed)
-				{
-					err = "stdin/stdout buffer overflow";
-					ret = 0;
-					stdout = null;
-					stderr = null;
-					return false;
-				}
-
-				ret = p.ExitCode;
-			}
-
-			stdout = stdSb.ToString();
-			stderr = errSb.ToString();
-
-			return true;
+			string cmdline = cmd + " " + args;
+			return GLib.Process.SpawnCommandLineSync(cmdline, out stdout, out stderr, out ret);
 		}
 
 		public static bool RunNotmuch(string args, out string output)
@@ -136,11 +24,10 @@ namespace NotMuchGUI
 			string stdout;
 			string stderr;
 			int ret;
-			string err;
 
-			if (RunCmd(MainClass.NotmuchExe, args, out stdout, out stderr, out ret, out err) == false)
+			if (RunCmd(MainClass.NotmuchExe, args, out stdout, out stderr, out ret) == false)
 			{
-				output = err;
+				output = "<failed to run command>";
 				return false;
 			}
 
